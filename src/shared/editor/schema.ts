2cebd50eef88ef0a16e88ca3b6bb1d304ea6b46f
PTPhongKMF <ptphongkmf@luxcadia.com>
2026-05-07 10:27:45 +0700

    fix(lexical): update lexical schema to use correct `SerializedEditorState` type
    Instead of checking with v.object, using v.custom and assign the type is better. This will ensure the parse process doesnt strip out keys that are not defined in the schema.

--- src/shared/editor/schema.ts
+++ src/shared/editor/schema.ts
@@ -1,5 +1,5 @@
 import * as v from "@valibot/valibot";
-import type { ElementFormatType, SerializedLexicalNode } from "lexical";
+import type { SerializedEditorState } from "lexical";
 
 export const EditorModes = {
   plaintext: "plain-text",
@@ -9,17 +9,21 @@ export const EditorModes = {
 
 export type EditorMode = typeof EditorModes[keyof typeof EditorModes];
 
-export const LexicalEditorAstSchema = v.object({
-  root: v.object({
-    type: v.literal("root"),
-    children: v.custom<SerializedLexicalNode[]>(
-      (val) => Array.isArray(val),
-    ),
-    direction: v.nullable(v.union([v.literal("ltr"), v.literal("rtl")])),
-    format: v.custom<ElementFormatType>(
-      (val) => typeof val === "string",
-    ),
-    indent: v.number(),
-    version: v.number(),
-  }),
-});
+// export const LexicalEditorAstSchema = v.object({
+//   root: v.object({
+//     type: v.literal("root"),
+//     children: v.custom<SerializedLexicalNode[]>(
+//       (val) => Array.isArray(val),
+//     ),
+//     direction: v.nullable(v.union([v.literal("ltr"), v.literal("rtl")])),
+//     format: v.custom<ElementFormatType>(
+//       (val) => typeof val === "string",
+//     ),
+//     indent: v.number(),
+//     version: v.number(),
+//   }),
+// });
+
+export const LexicalEditorAstSchema = v.custom<SerializedEditorState>((input) =>
+  typeof input === "object" && input !== null && "root" in input
+);
